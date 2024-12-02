e4fff72d1c6d0fde5462257117412bfdcf01541a
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-02 23:07:55 +0000

    Added simple design to html
    added a form and number input box as well as a
    submit input.

--- ticket1.js
+++ ticket1.js
@@ -1,7 +1,8 @@
 // Write a JavaScript function `convertGBPtoEUR` ✅
+// that takes in a specified amount in £, ✅
+// and converts it to € using a given exchange rate. ✅
+// The function should return the converted amount. ✅
+
 function convertGBPtoEUR (pounds) {
     return pounds*1.21
 }
-// that takes in a specified amount in £, 
-// and converts it to € using a given exchange rate. 
-// The function should return the converted amount.
