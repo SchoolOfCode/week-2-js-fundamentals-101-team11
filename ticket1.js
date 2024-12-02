2ea4040be4ceb2ac84e633e954e01f76abc6386f
BeardedGrinn <mitchell.t.grinnell@hotmail.co.uk>
2024-12-02 12:32:47 +0000

    Started working on first MVP

---
+++ ticket1.js
@@ -0,0 +1,7 @@
+// Write a JavaScript function `convertGBPtoEUR` 
+function () {
+    
+}
+// that takes in a specified amount in £, 
+// and converts it to € using a given exchange rate. 
+// The function should return the converted amount.
